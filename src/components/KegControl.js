5e604cb4acef07ddd4defc48d99f348f8d3aa5a2
Drake Wilcox <drakewilcox@gmail.com>
2020-05-01 13:56:31 -0700

    move kegList and begin to refactor to redux with problems.

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -2,7 +2,9 @@ import React from 'react';
 import NewKegForm from './KegFormControl/NewKegForm';
 import MainHeader from './MainHeader';
 import Beer from './KegDetails/Beer';
+import KegList from './KegList.js';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class KegControl extends React.Component {
 
@@ -11,89 +13,7 @@ class KegControl extends React.Component {
     this.state = {
       showHomePage: true,
 
-      kegList: [
-        {
-          name: "Briney Melon Gose",
-          brewery: "Anderson Valley",
-          abv: "4.2%",
-          origin: "CA",
-          pintPrice: 6, 
-          kegLevel: 124,
-          id: "7707701"
-        },
-        {
-          name: "Cheap and Cold",
-          brewery: "Hamm's",
-          abv: "4.7%",
-          origin: "USA",
-          pintPrice: 3, 
-          kegLevel: 124,
-          id: "7707702"
-        },
-        {
-          name: "Viva La Pineapple Cider",
-          brewery: "Reverend Nat's",
-          abv: "6%",
-          origin: "PDX",
-          pintPrice: 6, 
-          kegLevel: 124,
-          id: "7707703"
-        },
-        {
-          name: "Dystopia IPA",
-          brewery: "Grains of Wrath",
-          abv: "6.7%",
-          origin: "WA",
-          pintPrice: 6, 
-          kegLevel: 124,
-          id: "7707704"
-        },
-        {
-          name: "FanZine IPA",
-          brewery: "Fort George",
-          abv: "6.7%",
-          origin: "OR",
-          pintPrice: 6, 
-          kegLevel: 124,
-          id: "7707705"
-        },
-        {
-          name: "Pilsner",
-          brewery: "Chuckanut",
-          abv: "5%",
-          origin: "WA",
-          pintPrice: 5, 
-          kegLevel: 124,
-          id: "7707706"
-        },
-        {
-          name: "Dark Star Coffee Bourbon Stout",
-          brewery: "Fremont",
-          abv: "14.5%",
-          origin: "WA",
-          pintPrice: 9, 
-          kegLevel: 124,
-          id: "7707707"
-        },
-        {
-          name: "Pretty Hot Habenero Pils",
-          brewery: "Ross Island",
-          abv: "4.9%",
-          origin: "PDX",
-          pintPrice: 5, 
-          kegLevel: 124,
-          id: "7707708"
-        },
-        {
-          name: "Saison Dupont",
-          brewery: "Dupont",
-          abv: "6.5%",
-          origin: "BEL",
-          pintPrice: 6, 
-          kegLevel: 124,
-          id: "7707709"
-        },
-      ], 
+      kegList: KegList, 
       currentSelectedKeg: {}
     }
   }
@@ -105,13 +25,21 @@ class KegControl extends React.Component {
     this.setState({showHomePage: true});
   }
 
+  // handleChangingSelectedKeg = (id) => {
+  //   const selectedKeg = this.state.kegList.filter(keg =>keg.id === id)[0];
+  //   this.setState({
+  //     currentSelectedKeg: selectedKeg,
+  //     showHomePage: true
+  //   });
+  // }
   handleChangingSelectedKeg = (id) => {
-    const selectedKeg = this.state.kegList.filter(keg =>keg.id === id)[0];
+    const selectedKeg = this.props.kegList.filter(keg => keg.id === id)[0];
     this.setState({
-      currentSelectedKeg: selectedKeg,
+      currentSelectedKeg: selectedKeg, 
       showHomePage: true
-    });
+    })
   }
+
 
   handleSalesReport = (salesReport) => {
     console.log(salesReport.pintsSold)
@@ -205,5 +133,15 @@ class KegControl extends React.Component {
     )
   }
 }
-KegControl = connect()(KegControl)
+
+KegControl.propTypes = {
+  kegList: PropTypes.object
+};
+
+const mapStateToProps = state => {
+  return {
+    kegList: state
+  }
+}
+KegControl = connect(mapStateToProps)(KegControl)
 export default KegControl;
---
+++ src/components/KegList.js
@@ -0,0 +1,85 @@
+
+export default [
+  {
+    name: "Briney Melon Gose",
+    brewery: "Anderson Valley",
+    abv: "4.2%",
+    origin: "CA",
+    pintPrice: 6, 
+    kegLevel: 124,
+    id: "7707701"
+  },
+  {
+    name: "Cheap and Cold",
+    brewery: "Hamm's",
+    abv: "4.7%",
+    origin: "USA",
+    pintPrice: 3, 
+    kegLevel: 124,
+    id: "7707702"
+  },
+  {
+    name: "Viva La Pineapple Cider",
+    brewery: "Reverend Nat's",
+    abv: "6%",
+    origin: "PDX",
+    pintPrice: 6, 
+    kegLevel: 124,
+    id: "7707703"
+  },
+  {
+    name: "Dystopia IPA",
+    brewery: "Grains of Wrath",
+    abv: "6.7%",
+    origin: "WA",
+    pintPrice: 6, 
+    kegLevel: 124,
+    id: "7707704"
+  },
+  {
+    name: "FanZine IPA",
+    brewery: "Fort George",
+    abv: "6.7%",
+    origin: "OR",
+    pintPrice: 6, 
+    kegLevel: 124,
+    id: "7707705"
+  },
+  {
+    name: "Pilsner",
+    brewery: "Chuckanut",
+    abv: "5%",
+    origin: "WA",
+    pintPrice: 5, 
+    kegLevel: 124,
+    id: "7707706"
+  },
+  {
+    name: "Dark Star Coffee Bourbon Stout",
+    brewery: "Fremont",
+    abv: "14.5%",
+    origin: "WA",
+    pintPrice: 9, 
+    kegLevel: 124,
+    id: "7707707"
+  },
+  {
+    name: "Pretty Hot Habenero Pils",
+    brewery: "Ross Island",
+    abv: "4.9%",
+    origin: "PDX",
+    pintPrice: 5, 
+    kegLevel: 124,
+    id: "7707708"
+  },
+  {
+    name: "Saison Dupont",
+    brewery: "Dupont",
+    abv: "6.5%",
+    origin: "BEL",
+    pintPrice: 6, 
+    kegLevel: 124,
+    id: "7707709"
+  },
+]
+
