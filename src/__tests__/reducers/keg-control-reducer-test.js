ab46f222ec7a8f72ce22743f1b64ee46d8941b34
Drake Wilcox <drakewilcox@gmail.com>
2020-05-01 09:21:47 -0700

    write first test for keg-control-reducer and successfully fail.

---
+++ src/__tests__/reducers/keg-control-reducer-test.js
@@ -0,0 +1,10 @@
+import kegControlReducer from '../../reducers/keg-control-reducer';
+
+describe('kegControlReducer', () => {
+  
+  test('Should return default state if there is no action type passed into the reducer', () => {
+    test ('Should return default state if there is no action passed into the reducer', () => {
+      expect(kegControlReducer({}, {type: null })).toEqual({});
+    });
+  });
+})
