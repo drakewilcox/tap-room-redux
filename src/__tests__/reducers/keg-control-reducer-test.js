800e3f8a1f5653a49fefd646a72432e6a2d07013
Drake Wilcox <drakewilcox@gmail.com>
2020-05-01 09:42:56 -0700

    write test for adding new keg and confirm successful fail.

--- src/__tests__/reducers/keg-control-reducer-test.js
+++ src/__tests__/reducers/keg-control-reducer-test.js
@@ -1,8 +1,41 @@
 import kegControlReducer from '../../reducers/keg-control-reducer';
 
 describe('kegControlReducer', () => {
+  let action; 
+  const kegData = {
+    name: 'Pilsner',
+    brewery: 'Pfriem',
+    abv: '5.0%',
+    pintPrice: 5,
+    kegLevel: 124,
+    id: 1
+  };
 
   test ('Should return default state if there is no action passed into the reducer', () => {
       expect(kegControlReducer({}, {type: null })).toEqual({});
     });
+  
+  test ('Should successfully add new keg data to kegList', () => {
+    const {name, brewery, abv, pintPrice, kegLevel, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brewery: brewery,
+      abv: abv, 
+      pintPrice: pintPrice,
+      kegLevel: kegLevel, 
+      id: id
+    };
+    expect(kegControlReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brewery: brewery,
+        abv: abv, 
+        pintPrice: pintPrice,
+        kegLevel: kegLevel, 
+        id: id
+      }
+    });
   });
+  
+});
