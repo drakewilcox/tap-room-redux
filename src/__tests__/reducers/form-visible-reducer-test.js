ef49f3ae0ada9637f0a48cf595cada56a5481307
Drake Wilcox <drakewilcox@gmail.com>
2020-05-02 23:20:32 -0700

    write test for toggle form and confirm meaningful fail.

--- src/__tests__/reducers/form-visible-reducer-test.js
+++ src/__tests__/reducers/form-visible-reducer-test.js
@@ -5,4 +5,8 @@ describe("formVisibleReducer", () => {
   test("Should return default state if no action type is recognized", () => {
     expect(formVisibleReducer(undefined, { type: null })).toEqual(false);
   });
-})
+
+  test('Should toggle form visibility state to true', () => {
+    expect(formVisibleReducer(undefined, { type: 'TOGGLE_FORM '})).toEqual(true);
+  });
+});
