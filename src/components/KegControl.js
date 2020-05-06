2192d281bcd551366bf1f78bdb77cb1f8fcf3b5e
Drake Wilcox <drakewilcox@gmail.com>
2020-05-06 08:37:54 -0700

    refactor and clean up code.

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -3,18 +3,18 @@ import NewKegForm from './KegFormControl/NewKegForm';
 import TapList from './KegList';
 import MainHeader from './MainHeader';
 import Beer from './KegDetails/Beer';
-// import KegList from './KegList.js';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import * as a from './../actions/ActionTypes';
 import * as c from './../actions';
 
 function KegControl (props) {
-
 
 const { dispatch } = props;
 
 const handleAddingNewKegToList = (newKeg) => {
    dispatch(c.addKeg(newKeg));
+   dispatch(c.toggleForm());
   }
 
 const handleChangingSelectedKeg = (id) => {
@@ -22,13 +22,22 @@ const handleChangingSelectedKeg = (id) => {
     dispatch(c.selectedKeg(kegInfo))
 }
 
-const handleSalesReport = (id) => {
-  
-  const soldKeg = props.kegList[id];
-  dispatch(c.salesReport(soldKeg));
-    // dispatch(c.selectedKeg(kegInfo));
-    };
+const handleSalesReport = (props) => {
+  const {id, amtSold } = props;
+  console.log(amtSold);
+  const action ={
+    type: a.SALES_REPORT,
+    id: id,
+    amtSold: amtSold
+  }
+  dispatch(action);
+  handleChangingSelectedKeg(id);
+};
 
+const handleToggleForm = () => {
+  dispatch(c.toggleForm());
+}
+if(props.formVisibleOnPage){
   return (
       <React.Fragment>
         <div className='wrapper'>
@@ -46,14 +55,44 @@ const handleSalesReport = (id) => {
             beer = {props.selectedKeg}
             onNewSalesReport = {handleSalesReport}/>
           </div>
+    
           <div className='footer'>
           <NewKegForm 
           onNewKegFormCreation = {handleAddingNewKegToList}/>
+          <button onClick={handleToggleForm} ></button>
           </div>
         </div>
       </React.Fragment>
     )
   }
+else {
+  return (
+    <React.Fragment>
+    <div className='wrapper'>
+      <div className='header'>
+        <MainHeader/>
+      </div>
+      <div className='main'>
+      <TapList
+      tapList = {props.kegList}
+      changeSelectedKeg = {handleChangingSelectedKeg}
+      />,
+      </div>
+      <div className='sidebar'>
+      <Beer
+        beer = {props.selectedKeg}
+        onNewSalesReport = {handleSalesReport}
+      />
+      </div>
+      <div className='footer'>
+      <button onClick={handleToggleForm} >ADD NEW KEG</button>
+      </div>
+    </div>
+  </React.Fragment>
+  )
+  
+  }
+} 
 
 KegControl.propTypes = {
   KegList: PropTypes.object
